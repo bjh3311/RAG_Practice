1e2d29545fe175566dd2ad8063d6706e355a1a72
bjh3311 <bjh3311@naver.com>
2025-07-26 02:09:31 +0900

    Feat : CORS 및 에러 해결

--- app/api/v1/chatbot.py
+++ app/api/v1/chatbot.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 from app.schemas.chatbot import ChatRequestSchema, ChatResponseSchema
 
-router = APIRouter(prefix="/chatbot", tags=["chatbot"])
+router = APIRouter(tags=["chatbot"])
 
-@router.post("/", response_model=ChatResponseSchema)
+@router.post("/chatbot", response_model=ChatResponseSchema)
 async def chatbot_root(request: ChatRequestSchema):
     return ChatResponseSchema(message="보고서")
--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from app.api.router import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(api_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 개발 시 모든 도메인 허용
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
