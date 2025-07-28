0c61c80cf8cdc10a637b4ef8a47dc8641ba98450
bjh3311 <bjh3311@naver.com>
2025-07-28 20:35:53 +0900

    Feat : fastapi_mcp 삭제
     해당 패키지는 내 FastAPI 서버를 MCP 서버로 동작시켜서 MCP 클라이언트(Claude, Cursor)에서 사용할 수 있게 하는 패키지 였다
    
     내가 필요한건 mcp-use 패키지

--- app/main.py
+++ app/main.py
@@ -1,15 +1,10 @@
 from fastapi import FastAPI
 from app.api.router import api_router
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi_mcp import FastApiMCP
 
 app = FastAPI()
 app.include_router(api_router)
 
-mcp = FastApiMCP(app)
-
-# Mount the MCP server directly to your FastAPI app
-mcp.mount()
 
 app.add_middleware(
     CORSMiddleware,
